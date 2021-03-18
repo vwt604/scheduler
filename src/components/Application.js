63a6fab90a402740e12dba039c95777b25294c55
Vanessa Yeung <vanessayeung@Vanessas-MacBook-Air.local>
2021-03-18 12:21:40 -0700

    update to retrieve appointments from API

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,65 +3,7 @@ import axios from 'axios';
 import "components/Application.scss";
 import DayList from 'components/DayList.js';
 import Appointment from "components/Appointment/index";
-
-
-// Test data 
-
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "3pm",
-    interview: {
-      student: "Amy Mansell",
-      interviewer: {
-        id: 2,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  }, 
-  {
-    id: 4,
-    time: "2pm",
-    interview: {
-      student: "Joe Doe",
-      interviewer: {
-        id: 1,
-        name: "Tori Malcolm",
-        avatar: "https://i.imgur.com/Nmx0Qxo.png",
-      }
-    }
-  }, 
-  {
-    id: 5,
-    time: "5pm",
-    interview: {
-      student: "Karen Jones",
-      interviewer: {
-        id: 5,
-        name: "Sven Jones",
-        avatar: "https://i.imgur.com/twYrpay.jpg",
-      }
-    }
-  }
-];
-
+import { getAppointmentsForDay } from "../helpers/selectors";
 
 export default function Application(props) {
 
@@ -72,28 +14,37 @@ export default function Application(props) {
     appointments: {}
   });
 
-  // Creating object with keys of state
-  // const state = { day: "Monday", days: [] };
-  // setState({ ...state, day: "Tuesday" });
+  // Updates the state with appointments for the day
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+  const appointments = dailyAppointments.map((appointment) => {
+    return <Appointment key={appointment.id} {...appointment} />;
+  });
+
 
   // Updates the state with the new day
   const setDay = day => setState({ ...state, day });
 
-  // Updates day state
-  const setDays = days => setState(prev => ({ ...prev, days }));
 
-  // Fetch day data from API
+  // Fetch data from API
   useEffect(() => {
-    axios.get('http://localhost:8001/api/days')
-      .then(res => {setDays([...res.data])})
-  }, []); 
+    Promise.all([
+      axios.get("http://localhost:8001/api/days"),
+      axios.get("http://localhost:8001/api/appointments"),
+      axios.get("http://localhost:8001/api/interviewers")
+    ])
+    .then((all) => {
+      setState((prev) => ({
+        ...prev,
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data,
+      }));
+        console.log(all[0].data, all[1].data, all[2].data);
+        const [first, second, third] = all;
+        console.log(first, second, third);
+      });
+    }, []);
 
-
-  const appointmentsArr = appointments.map(appointment => {
-    return (
-      <Appointment key={appointment.id} {...appointment} />
-    );
-  });
 
     return (
       <main className="layout">
@@ -118,7 +69,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {appointmentsArr}
+        {appointments}
         <Appointment key="last" time="5pm" />
       </section>
     </main>
