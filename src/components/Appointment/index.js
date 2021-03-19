a3b9dfd8770a7fd6107eb49bb33630784bc29684
Vanessa Yeung <vanessayeung@Vanessas-MacBook-Air.local>
2021-03-18 19:20:50 -0700

    implement visual modes at index.js

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,14 +4,34 @@ import 'components/Appointment/styles.scss'
 import Header from 'components/Appointment/Header.js';
 import Show from 'components/Appointment/Show.js';
 import Empty from 'components/Appointment/Empty.js';
+import Form from 'components/Appointment/Form.js';
+import useVisualMode from 'hooks/useVisualMode.js';
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
-  const { time, interview } = props;
+  const { mode, transition, back } = useVisualMode(
+		props.interview ? SHOW : EMPTY
+	);
 
-  return(
+  return (
     <article className="appointment">
-      <Header time={time} />
-      { interview ? <Show student={interview.student} interviewer={interview.interviewer}/> : <Empty /> }
+    <Header time={props.time} /> 
+    {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />} 
+    {mode === CREATE && 
+      <Form
+        interviewers={[]}
+        onCancel={() => back()}
+      />
+    }
+    {mode === SHOW && (
+      <Show
+        student={props.interview.student}
+        interviewer={props.interview.interviewer}
+      />
+    )}
     </article>
-  );
+  )
 };
